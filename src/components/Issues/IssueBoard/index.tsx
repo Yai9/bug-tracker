1c0fef46ea2a9df7e85c07247af7e92a34ecc159
yai <arsenovlidija02@gmail.com>
2022-02-04 20:36:47 +0100

    type modifications

--- src/components/Issues/IssueBoard/index.tsx
+++ src/components/Issues/IssueBoard/index.tsx
@@ -1,29 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import IssueItem from '../IssueItem/index';
 
-import { Report } from '../../../types/Report/Report';
 import * as styles from './index.module.css';
 
 const IssueBoard: React.FC = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState<any[]>([]);
 
-    useEffect(()=>{
+    useEffect((): void => {
 
-        axios.get('http://localhost:3001/api/reports').then(res=>{
+        axios.get('http://localhost:3001/api/reports').then(res => {
             const data = res.data;
             setData(data);
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err)
         })
-    },[])
-   
+    }, [])
+
     return (
         <div className={styles.issueBoardWrapper}>
             <div className={styles.issueBoard}>
                 <h1 className={styles.issueBoardTitle}>Bug Records</h1>
-                {data.length !== 0 && data.map(data=>(
-                    <IssueItem title={ data.title } severity={data.severity}/>
+                {data.length !== 0 && data.map(data => (
+                    <IssueItem title={data.title} severity={data.severity} />
                 ))}
             </div>
 
